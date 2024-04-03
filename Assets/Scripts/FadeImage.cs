753c0a8f91ba8ddf86c69a9b8386f073caacbbf8
Aden Robertson <alrob2224@gmail.com>
2024-04-03 13:47:25 -0400

    Finished my level

---
+++ Assets/Scripts/FadeImage.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RevealImage : MonoBehaviour
+{
+    Image image;
+    Color color;
+
+    private void Start()
+    {
+        image = GetComponent<Image>();
+        color = image.color;
+
+        color.a = 1;
+        image.color = color;
+    }
+
+    public float targetTime;
+    private float targetTime2 = 0.1f;
+
+    void Update()
+    {
+        targetTime -= Time.deltaTime;
+
+        if (targetTime <= 2.0f)
+        {
+            targetTime2 -= Time.deltaTime;
+
+            if (targetTime2 <= 0.0f)
+            {
+                color.a -= 0.05f;
+                image.color = color;
+
+                targetTime2 = 0.1f;
+            }
+        }
+
+        if (color.a <= 0)
+        {
+            image.enabled = false;
+            Debug.Log("Image inactive here");
+        }
+    }
+}
