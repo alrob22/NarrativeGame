aec5e79054fb87d001656df0ee6dbff02f52dfe9
BlueClueCoder <elliott@januzelli.net>
2024-05-02 08:20:18 -0400

    Elliott Level Done
    Everything should be good to go

--- Assets/Scripts/ChangeSprite.cs
+++ Assets/Scripts/ChangeSprite.cs
@@ -7,6 +7,11 @@ public class ChangeSprite : MonoBehaviour
 
     public Sprite sp1, sp2;
     public static int currSprite = 0;
+    public GameObject enemy0;
+    public GameObject enemy1;
+    public GameObject enemy2;
+    public GameObject enemy3;
+    public GameObject enemy4;
 
     // Start is called before the first frame update
     void Start()
@@ -23,12 +28,75 @@ public class ChangeSprite : MonoBehaviour
             {
                 GetComponent<SpriteRenderer>().sprite = sp2;
                 currSprite = 1;
-                transform.localScale = new Vector3(.4f, .4f, 1);
+                transform.localScale = new Vector3(.8f, .8f, 1);
             } else
             {
                 GetComponent<SpriteRenderer>().sprite = sp1;
                 currSprite = 0;
-                transform.localScale = new Vector3(.6f, .6f, 1);
+                transform.localScale = new Vector3(2f, 2f, 1);
+            }
+        }
+
+
+        //check if hit enemies
+        if (Vector2.Distance(transform.position, enemy0.transform.position) <= 1.5f)
+        {
+            //if haku form (move this chunk into changesprite script)
+            if (currSprite == 0)
+            {
+                transform.position = new Vector3(6.91f, 7.8f, -1f);
+
+            } else
+            {
+                Destroy(enemy0);
+            }
+        }
+        if (Vector2.Distance(transform.position, enemy1.transform.position) <= 1.5f)
+        {
+            if (currSprite == 0)
+            {
+                transform.position = new Vector3(6.91f, 7.8f, -1f);
+
+            }
+            else
+            {
+                Destroy(enemy1);
+            }
+        }
+        if (Vector2.Distance(transform.position, enemy2.transform.position) <= 3f)
+        {
+            if (currSprite == 0)
+            {
+                transform.position = new Vector3(6.91f, 7.8f, -1f);
+
+            }
+            else
+            {
+                Destroy(enemy2);
+            }
+        }
+        if (Vector2.Distance(transform.position, enemy3.transform.position) <= 1.5f)
+        {
+            if (currSprite == 0)
+            {
+                transform.position = new Vector3(6.91f, 7.8f, -1f);
+
+            }
+            else
+            {
+                Destroy(enemy3);
+            }
+        }
+        if (Vector2.Distance(transform.position, enemy4.transform.position) <= 2f)
+        {
+            if (currSprite == 0)
+            {
+                transform.position = new Vector3(6.91f, 7.8f, -1f);
+
+            }
+            else
+            {
+                Destroy(enemy4);
             }
         }
     }
